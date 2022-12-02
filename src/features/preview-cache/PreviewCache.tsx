0970322938c9bcc405b83650bc8a101b8c3a3437
laszloekovacs <laszloekovacs@gmail.com>
2022-12-02 14:24:12 +0100

    cache seems to work now

--- src/features/preview-cache/PreviewCache.tsx
+++ src/features/preview-cache/PreviewCache.tsx
@@ -3,30 +3,33 @@ import { useSelector } from "react-redux"
 import { workspaceContext } from "../workspace/Workspace"
 import { Store } from "../../store/store"
 import produce from "immer"
+import { idText } from "typescript"
 
-function PreviewCache() {
+function PreviewCache({ children }) {
     const store = useSelector((s: Store) => s)
     const [previewStore, setPreviewStore] = useState({})
     const workDirectory = useContext(workspaceContext)
-    //   const
+    const [panoramaDirectory, setPanoramaDirectory] = useState<FileSystemDirectoryHandle | null>(null)
+    const [assetsDirectory, setAssetsDirectory] = useState<FileSystemDirectoryHandle | null>(null)
     const [cache, setCache] = useState(new Map())
 
     /* open directories only once */
     /* check if we already loaded the image, otherwise cache it */
-    async function resolvePanorama(workDirectory: FileSystemDirectoryHandle, cache: Map<string, string>, panorama: string) {
+    async function resolvePanorama(dir: FileSystemDirectoryHandle, cache: Map<string, string>, panorama: string) {
         console.log("resolving")
         try {
             if (cache.has(panorama)) {
                 return cache.get(panorama)
-            }
-            console.log(workDirectory)
-            const assets = await workDirectory.getDirectoryHandle("assets")
+            } else {
+                /* not in cache, needs to load */
+                const file = await (await dir.getFileHandle(panorama)).getFile()
+                const url = URL.createObjectURL(file)
 
-            return ""
+                cache.set(panorama, url)
+                return url
+            }
         } catch (error) {
-            return ""
-            console.log("failed?")
-            console.log(error)
+            return "error"
         }
     }
 
@@ -35,16 +38,33 @@ function PreviewCache() {
             const updatedStore = produce(store, async (draft) => {
                 /* go trough each scene, find panoramas */
                 for (let scene of Object.values(draft.scenes)) {
-                    scene.panorama = (await resolvePanorama(workDirectory, blobCache, scene.panorama)) || ""
+                    if (panoramaDirectory != null)
+                        scene.panorama = (await resolvePanorama(panoramaDirectory, blobCache, scene.panorama)) || ""
                 }
             })
 
+            setCache(blobCache)
             return updatedStore
         } catch (error) {
             console.log(error)
             return store
         }
     }
+
+    /* make sure we have these ready */
+    useEffect(() => {
+        ;(async () => {
+            let assets = assetsDirectory || (await workDirectory?.getDirectoryHandle("assets"))
+            let panorama = panoramaDirectory || (await assets?.getDirectoryHandle("panorama"))
+
+            if (assets && panorama) {
+                setAssetsDirectory(assets)
+                setPanoramaDirectory(panorama)
+            } else {
+                throw "failed to open assets and panorama directory"
+            }
+        })()
+    }, [])
 
     /* make a copy of the fresh store, and change paths to blobs */
     useEffect(() => {
@@ -57,7 +77,7 @@ function PreviewCache() {
         }
     }, [store])
 
-    return <div>PreviewCache</div>
+    return <div>{children}</div>
 }
 
 export default PreviewCache
