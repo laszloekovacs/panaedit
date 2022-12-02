03b0f0af96c7cdab6ea936520ffe7dfe45b8ac67
laszloekovacs <laszloekovacs@gmail.com>
2022-12-02 11:20:11 +0100

    worked on scene list

--- src/features/scene-list/SceneList.tsx
+++ src/features/scene-list/SceneList.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React from "react"
+import { useSelector } from "react-redux"
+import type { Store } from "../../store/store"
+import SceneListItem from "./SceneListItem"
 
-const dummyScenes = ["ab - vezérigazgató", "ba - előszoba", "fg - folyosó", "er- öltöző", "dfs - konyha", "er - iroda"];
+// @ts-ignore
+import styles from "./scenelist.module.css"
+
+const dummyScenes = ["ab - vezérigazgató", "ba - előszoba", "fg - folyosó", "er- öltöző", "dfs - konyha", "er - iroda"]
 
 function SceneList() {
-    const list = dummyScenes.map((item) => {
-        return (
-            <li key={item}>
-                <button>{item}</button>
-            </li>
-        );
-    });
+    const scenes = useSelector((store: Store) => store?.scenes) || {}
 
-    return <ul>{list}</ul>;
+    const list = Object.keys(scenes).map((item) => {
+        return <SceneListItem key={item} title={scenes[item].title} item={item} onClick={() => {}} />
+    })
+
+    return <ul className={styles.container}>{list}</ul>
 }
 
-export default SceneList;
+export default SceneList
