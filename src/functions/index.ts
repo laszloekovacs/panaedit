b57e0ddc8ab85b6f3d4c0d310fa9dabca7e32167
laszloekovacs <laszloekovacs@gmail.com>
2023-04-04 21:36:34 +0200

    created load project

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,2 @@
 export * from './selectWorkFolder'
+export * from './loadProjectFileDialog'
---
+++ src/functions/loadProjectFileDialog.ts
@@ -0,0 +1,16 @@
+const fileOptions = {
+	types: [{ description: 'json document', accept: { 'application/json': ['.json'] } }]
+}
+
+export async function loadProjectFileDialog() {
+	const [filehandle] = await window.showOpenFilePicker(fileOptions)
+
+	const file = await filehandle.getFile()
+	const text = await file.text()
+
+	if (!file || !text) return
+
+	console.log(JSON.parse(text))
+
+	// dispatch action to load project
+}
