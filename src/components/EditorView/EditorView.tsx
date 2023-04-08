d48809eb1ec953cc7786e0a5d88e0aa85ab3db0d
laszloekovacs <laszloekovacs@gmail.com>
2023-04-08 18:56:51 +0200

    changed directory name to capitalized

--- src/components/EditorView/EditorView.tsx
+++ src/components/EditorView/EditorView.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../SidebarB/Sidebar'
+import Sidebar from '../Sidebar/Sidebar'
 import Pannellum from '../Pannellum/Pannellum'
 
 const EditorView = () => {
